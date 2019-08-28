cdc3d79864390b874865a07a7abd371a86b7444f
Edison Klafke Fillus <edison.fillus@gmail.com>
2019-08-27 22:37:05 -0300

    include purecss and start screen

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import 'purecss';
+import './css/side-menu.css';
+//import './js/ui.js';
 
 ReactDOM.render(
   <App />,
